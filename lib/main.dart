3a88bf83e15cc505fe9b28378d723a739f45dade
zhangxu <zhangxusy@foxmail.com>
2019-08-05 16:39:04 +0800

    增加主函数入口代码 增加引导页

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,24 @@
 import 'package:flutter/material.dart';
+import 'pages/index_page.dart';
 
-void main() => runApp();
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      child:MaterialApp(
+        title: '百姓生活+',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.pink
+        ),
+       home: IndexPage(),
+      ) ,
+
+    );
+  }
+}
+
 
---
+++ lib/pages/index_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class IndexPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      
+      appBar: AppBar(
+        title: Text(
+          '测试'
+        ),
+      ),
+      body: Center(
+        child: Text('引导页'),
+      ),
+    );
+  }
+}
