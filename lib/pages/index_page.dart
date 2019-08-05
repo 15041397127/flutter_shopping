982e6468dc36a6f0421cd0537d6a5dd6d81593fa
zhangxu <zhangxusy@foxmail.com>
2019-08-05 16:54:47 +0800

    增加底部导航栏图标文字数组

--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
-class IndexPage extends StatelessWidget {
+import 'package:flutter/cupertino.dart';
+
+class IndexPage extends StatefulWidget {
+  @override
+  _IndexPageState createState() {
+    return _IndexPageState();
+  }
+}
+
+class _IndexPageState extends State<IndexPage> {
+
+  final List<BottomNavigationBarItem> bottomTabs = [
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),
+      title: Text('首页'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.search),
+      title: Text('分类'),
+    ),
+
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.shopping_cart),
+      title: Text('购物车'),
+    ),
+
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.profile_circled),
+      title: Text('会员中心'),
+    )
+
+  ];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      
-      appBar: AppBar(
-        title: Text(
-          '测试'
-        ),
-      ),
-      body: Center(
-        child: Text('引导页'),
-      ),
-    );
+    return null;
   }
 }
