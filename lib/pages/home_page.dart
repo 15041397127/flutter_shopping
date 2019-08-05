ff6440fe9fab102b3d53b3b3a7440092576ea7ac
zhangxu <zhangxusy@foxmail.com>
2019-08-05 21:22:16 +0800

    Dio请求小案例

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,28 +2,115 @@ import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'dart:async';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController typeController = TextEditingController();
+  String showText = '欢迎您';
+
   @override
   Widget build(BuildContext context) {
-    gethttp();
     // TODO: implement build
-    return Center(
-      child: Text('首页'),
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('首页测试Dio'),
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: typeController,
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(10.0),
+                    labelText: '美女类型',
+                    helperText: '请输入你喜欢的类型'),
+                autofocus: false, //关闭第一响应者  键盘不默认打开
+              ),
+              RaisedButton(child: Text('选择完毕'), onPressed: _choiceAction,),
+              Text(
+                showText,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
 
-  void gethttp() async {
-    try {
-      Response response;
+  void _choiceAction(){
 
-      response = await Dio().get('https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=子望');
+    print('开始选择');
 
-      return  print(response);
+    if(typeController.text.toString()==''){
 
-    } catch (e) {
-      return debugPrint(e);
+      showDialog(
+          context: context,
+          builder: (context)=>AlertDialog(title: Text('类型不能为空'))
+      );
+
+
+
+    }else{
+
+      getHttp(typeController.text.toString()).then((val){
+
+        setState(() {
+          showText = val['data']['name'].toString();
+        });
+
+      });
+
     }
+
   }
 
+  Future getHttp(String TypeTest) async {
+    try {
+      Response response;
+      var data = {'name': TypeTest};
+
+      response = await Dio().get(
+          'https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian',
+          queryParameters: data);
+
+      return response.data;
+    } catch (e) {
+      print(e);
+    }
+  }
 }
+
+//class HomePage extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    gethttp();
+//    // TODO: implement build
+//    return Center(
+//      child: Text('首页'),
+//    );
+//  }
+//
+//
+//  void gethttp() async {
+//    try {
+//      Response response;
+//
+//      response = await Dio().get('https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=子望');
+//
+//      return  print(response);
+//
+//    } catch (e) {
+//      return debugPrint(e);
+//    }
+//  }
+//
+//}
