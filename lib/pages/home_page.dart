8478b6d76f4eda264a60e4fb04216d7c45e87fa4
zhangxu <zhangxusy@foxmail.com>
2019-08-05 20:35:15 +0800

    添加dio依赖 测试dio get请求

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'dart:async';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    gethttp();
     // TODO: implement build
     return Center(
       child: Text('首页'),
     );
   }
+
+
+  void gethttp() async {
+    try {
+      Response response;
+
+      response = await Dio().get('https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=子望');
+
+      return  print(response);
+
+    } catch (e) {
+      return debugPrint(e);
+    }
+  }
+
 }
