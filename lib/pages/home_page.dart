9454a0ef52ccafafd9736c25235aff405d6b52b7
zhangxu <zhangxusy@foxmail.com>
2019-08-06 14:38:04 +0800

    使用screenutil 做屏幕适配

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../service/service_method.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:convert';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -52,7 +53,11 @@ class _HomePageState extends State<HomePage> {
                   );
                 }else{
                   return Center(
-                    child: Text('加载中....'),
+                    child: Text(
+                      '加载中....'
+//                  style: TextStyle(
+//                    fontSize:ScreenUtil().setSp(28,false),
+                  ),
                   );
                 }
           },
@@ -73,13 +78,24 @@ class SwiperDiy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    //屏幕适配 根据iphone6
+    ScreenUtil.instance = ScreenUtil(
+      width: 750,
+      height: 1334
+    )..init(context);
+
+    print('设备像素密度:${ScreenUtil.pixelRatio}');
+    print('设备高:${ScreenUtil.screenHeight}');
+    print('设备宽:${ScreenUtil.screenWidth}');
+
 //     arr = ['http://wx4.sinaimg.cn/large/7695e2e2ly1g5pnrmegv6j20m80f5taf.jpg',
 //           'http://wx4.sinaimg.cn/large/7695e2e2ly1g5pnrm4b9vj212z0lltdf.jpg',
 //           'http://wx3.sinaimg.cn/large/006FCqYVgy1g5pu3o5phzj31910u07ab.jpg'
 //     ];
     // TODO: implement build
     return Container(
-      height: 333,
+      height: ScreenUtil().setHeight(333),
+      width: ScreenUtil().setWidth(750),
       child: Swiper(
 
         itemCount:swiperDateList.length,
