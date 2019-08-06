df163a8e7ceea69f8416a7c9f8ecf9a7c5239364
zhangxu <zhangxusy@foxmail.com>
2019-08-06 16:35:44 +0800

    增加url_launcher 吊起本地电话和跳转web功能 添加拨打电话功能

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import '../service/service_method.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:convert';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,7 +13,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   String homePageContent = '正在获取数据';
 
 //  @override
@@ -31,62 +31,67 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      
       appBar: AppBar(
         title: Text('首页'),
         elevation: 0.0,
       ),
-      
+
       //SingleChildScrollView与listView冲突
       //使用FutureBuilder 数据驱动视图
       body: FutureBuilder(
-         future: getHomePageContent(),
-          builder: (context,snapshot){
-                if(snapshot.hasData){
-                  var data = json.decode(snapshot.data.toString());
-                  List<Map> swiper = (data['data']['slides']as List).cast();
-                  List<Map> navgatorList = (data['data']['category']as List).cast();
-                  String adPicture = data['data']['advertesPicture']['PICTURE_ADDRESS'];
-                  return Column(
-                    children: <Widget>[
-                      SwiperDiy(swiperDateList: swiper,),
-                      TopNavigator(navigatorList: navgatorList,),
-                      AdBanner(adPicture: adPicture),
-                    ],
+        future: getHomePageContent(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var data = json.decode(snapshot.data.toString());
+            List<Map> swiper = (data['data']['slides'] as List).cast();
+            List<Map> navgatorList = (data['data']['category'] as List).cast();
+            String adPicture =
+                data['data']['advertesPicture']['PICTURE_ADDRESS'];
+            String leaderImage = data['data']['shopInfo']['leaderImage'];
+            String leaderPhone = data['data']['shopInfo']['leaderPhone'];
 
-                  );
-                }else{
-                  return Center(
-                    child: Text(
-                      '加载中....'
+            return Column(
+              children: <Widget>[
+                SwiperDiy(
+                  swiperDateList: swiper,
+                ),
+                TopNavigator(
+                  navigatorList: navgatorList,
+                ),
+                AdBanner(adPicture: adPicture),
+                CallPhone(
+                  leaderImage: leaderImage,
+                  leaderPhone: leaderPhone,
+                ),
+              ],
+            );
+          } else {
+            return Center(
+              child: Text('加载中....'
 //                  style: TextStyle(
 //                    fontSize:ScreenUtil().setSp(28,false),
                   ),
-                  );
-                }
-          },
+            );
+          }
+        },
       ),
     );
   }
 }
-
 
 /***
  *首页轮播组件
  *
  */
 class SwiperDiy extends StatelessWidget {
+  final List swiperDateList;
 
-   final List swiperDateList;
 //  SwiperDiy({Key key,this.swiperDateList}):super(key:key);
-  SwiperDiy({this.swiperDateList});//目前可以省略这么写 构造函数
+  SwiperDiy({this.swiperDateList}); //目前可以省略这么写 构造函数
 
 //   List arr = List();
   @override
   Widget build(BuildContext context) {
-
-
-
     print('设备像素密度:${ScreenUtil.pixelRatio}');
     print('设备高:${ScreenUtil.screenHeight}');
     print('设备宽:${ScreenUtil.screenWidth}');
@@ -100,16 +105,16 @@ class SwiperDiy extends StatelessWidget {
       height: ScreenUtil().setHeight(333),
       width: ScreenUtil().setWidth(750),
       child: Swiper(
-
-        itemCount:swiperDateList.length,
-        itemBuilder: (BuildContext context,int index){
-            return Image.network('${swiperDateList[index]['image']}',fit: BoxFit.fill,);
+        itemCount: swiperDateList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            '${swiperDateList[index]['image']}',
+            fit: BoxFit.fill,
+          );
         },
         pagination: SwiperPagination(),
         autoplay: true,
-
       ),
-
     );
   }
 }
@@ -118,30 +123,30 @@ class SwiperDiy extends StatelessWidget {
  * 顶部导航组件
  */
 class TopNavigator extends StatelessWidget {
-
   final List navigatorList;
+
   TopNavigator({this.navigatorList});
 
-  Widget _gridViewItemUI(BuildContext context,item){
-
+  Widget _gridViewItemUI(BuildContext context, item) {
     return InkWell(
-      onTap: (){
+      onTap: () {
         print('点击了导航');
       },
       child: Column(
         children: <Widget>[
-          Image.network(item['image'],width: ScreenUtil().setWidth(95),),
+          Image.network(
+            item['image'],
+            width: ScreenUtil().setWidth(95),
+          ),
           Text(item['mallCategoryName']),
         ],
       ),
-
     );
   }
 
   @override
   Widget build(BuildContext context) {
-
-    if(this.navigatorList.length>10){
+    if (this.navigatorList.length > 10) {
       this.navigatorList.removeRange(10, this.navigatorList.length);
     }
 
@@ -149,24 +154,23 @@ class TopNavigator extends StatelessWidget {
     return Container(
       height: ScreenUtil().setHeight(250),
       padding: EdgeInsets.all(3.0),
-      child:GridView.count(
-          crossAxisCount: 5,
-          padding: EdgeInsets.only(left: 5.0,right: 5.0,top: 5.0,bottom: 1.0),
-          children: navigatorList.map((item){
-            return _gridViewItemUI(context, item);
-          }).toList(),
+      child: GridView.count(
+        crossAxisCount: 5,
+        padding: EdgeInsets.only(left: 5.0, right: 5.0, top: 5.0, bottom: 1.0),
+        children: navigatorList.map((item) {
+          return _gridViewItemUI(context, item);
+        }).toList(),
       ),
     );
   }
 }
 
-
 /**
  * 广告区域
  */
 class AdBanner extends StatelessWidget {
-
   final String adPicture;
+
   AdBanner({this.adPicture});
 
   @override
@@ -178,13 +182,40 @@ class AdBanner extends StatelessWidget {
   }
 }
 
+/**
+ * 拨打电话功能
+ */
+class CallPhone extends StatelessWidget {
+  //店长图片
+  final String leaderImage;
 
+  //店长电话
+  final String leaderPhone;
 
+  CallPhone({this.leaderImage, this.leaderPhone});
 
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      child: InkWell(
+        onTap: _launchURL,
+        child: Image.network(leaderImage),
+      ),
+    );
+  }
 
-
-
-
+  void _launchURL() async {
+    String url = 'tel:'+leaderPhone;
+//    String url = 'https://flutterchina.club/';
+    print(url);
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'url不能访问异常';
+    }
+  }
+}
 
 //测试方法
 /*
