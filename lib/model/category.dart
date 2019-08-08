f2481a7af3439d3016c2a12ba17ec791cc682aa2
zhangxu <zhangxusy@foxmail.com>
2019-08-08 16:13:18 +0800

    使用json_to_dart快速生产model https://javiercbk.github.io/json_to_dart/

--- lib/model/category.dart
+++ lib/model/category.dart
@@ -38,3 +38,14 @@ class CategoryListModel{
     );
   }
 }
+
+
+/**
+ * json_to_dart的使用
+
+    如果我们得到一个特别复杂的JSON,有时候会无从下手开始写Model,这时候就可以使用一些辅助工具。我认为json_to_dart是比较好用的一个。它可以直接把json转换成dart类，然后进行一定的修改，就可以快乐的使用了。工作中我拿到一个json，都是先操作一下，然后再改的。算是一个小窍门吧。
+
+    请记住网址：
+
+    https://javiercbk.github.io/json_to_dart/
+ */
