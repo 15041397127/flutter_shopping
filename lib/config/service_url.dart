a1decab6aa02e2287497e5211c72802626fe617c
zhangxu <zhangxusy@foxmail.com>
2019-08-08 15:16:40 +0800

    测试分类页接口

--- lib/config/service_url.dart
+++ lib/config/service_url.dart
@@ -2,5 +2,6 @@ const serviceUrl= 'https://wxmini.baixingliangfan.cn/baixing/';
 const servicePath = {
   'homePageContent':serviceUrl + 'wxmini/homePageContent',//商店首页信息
   'homePageBelowContent':serviceUrl + 'wxmini/homePageBelowConten',//商城首页热卖商品
+  'getCategory':serviceUrl + 'wxmini/getCategory',//商品类别信息
 
 };
--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,11 +1,47 @@
 import 'package:flutter/material.dart';
+import '../service/service_method.dart';
+import 'dart:convert';
 
-class CateGoryPage extends StatelessWidget {
+
+class CateGoryPage extends StatefulWidget {
+  @override
+  _CateGoryPageState createState() {
+    return _CateGoryPageState();
+  }
+}
+
+class _CateGoryPageState extends State<CateGoryPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Center(
-      child: Text('分类'),
+    _getCategory();
+    return Container(
+      child: Center(
+        child: Text('分类页面'),
+      ),
     );
+  }
+  
+  
+  void _getCategory()async{
+    
+    await request('getCategory').then((val){
+      
+      var data = json.decode(val.toString());
+
+      print(data);
+      
+    });
+    
   }
 }
