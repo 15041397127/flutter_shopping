900b2656cf60b93e03e0ee64726894f37efaa362
zhangxu <zhangxusy@foxmail.com>
2019-08-12 21:33:07 +0800

    数组中 添加'全部'类型

--- lib/provide/child_category.dart
+++ lib/provide/child_category.dart
@@ -4,8 +4,18 @@ import '../model/category.dart';
 class ChildCategory with ChangeNotifier {
   List<BxMallSubDto> childCategoryList = [];
 
-  getChildCategory(List list) {
-    childCategoryList = list;
+  getChildCategory(List<BxMallSubDto> list) {
+
+    BxMallSubDto bxMallSubDto = BxMallSubDto();
+
+    bxMallSubDto.mallSubId = '00';
+    bxMallSubDto.mallCategoryId = '00';
+    bxMallSubDto.comments = 'null';
+    bxMallSubDto.mallSubName = '全部';
+
+    childCategoryList = [bxMallSubDto];
+
+    childCategoryList.addAll(list);
     notifyListeners();
   }
 }
