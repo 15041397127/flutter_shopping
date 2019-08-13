d61fd33fb875dde25a5c43aa7cadd33105e10c7c
zhangxu <zhangxusy@foxmail.com>
2019-08-13 10:34:40 +0800

    main函数 添加对CategoryGoodListProvide进行绑定

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,19 @@ import 'pages/index_page.dart';
 import 'package:provide/provide.dart';
 import 'provide/counter.dart';
 import 'provide/child_category.dart';
+import 'provide/category_goods_list.dart';
 
 void main(){
 
   var counter = Counter();
   var childCategory = ChildCategory();
+  var goodsDetailList = CategoryGoodListProvide();
   var proivders = Providers();
   proivders
            ..provide(Provider<Counter>.value(counter))
-           ..provide(Provider<ChildCategory>.value(childCategory));//注册依赖 绑定各个类可多个
+           ..provide(Provider<ChildCategory>.value(childCategory))//注册依赖 绑定各个类可多个
+           ..provide(Provider<CategoryGoodListProvide>.value(goodsDetailList));//商品详情绑定
+
 
   runApp(ProviderNode(child:MyApp(),providers:proivders));
 }
