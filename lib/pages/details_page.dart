03cdcded8461750a1c6c71b455828c78b9d3839e
zhangxu <zhangxusy@foxmail.com>
2019-08-14 20:57:54 +0800

    详情页UI 框架搭建

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -2,45 +2,92 @@ import 'package:flutter/material.dart';
 import 'package:provide/provide.dart';
 import '../provide/details_info.dart';
 
-class DetailPage extends StatefulWidget {
-
+class DetailPage extends StatelessWidget {
   final String goodsId;
+
   DetailPage(this.goodsId);
 
   @override
-  _DetailPageState createState() {
-    return _DetailPageState();
-  }
-}
-
-class _DetailPageState extends State<DetailPage> {
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    _getBackInfo(context);
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: Text('详情页面'),),
-      body: Center(child: Text('详情页面商品详情${widget.goodsId}'),),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text('商品详情'),
+      ),
+      body: FutureBuilder(
+        future: _getBackInfo(context),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Container(
+              child: Column(
+                children: <Widget>[
+                  Text('哈哈哈'),
+                ],
+              ),
+            );
+          } else {
+            return Text(
+              '加载中....',
+            );
+          }
+        },
+      ),
     );
   }
 
+  Future _getBackInfo(BuildContext context) async {
+    await Provide.value<DetailInfoProvide>(context).getGoodsInfor(goodsId);
+    return '完成加载';
+  }
+}
 
-  void _getBackInfo(BuildContext context)async{
+/**
+ * class DetailPage extends StatefulWidget {
+
+    final String goodsId;
+    DetailPage(this.goodsId);
+
+    @override
+    _DetailPageState createState() {
+    return _DetailPageState();
+    }
+    }
+
+    class _DetailPageState extends State<DetailPage> {
+
+    @override
+    void initState() {
+    super.initState();
+    }
+
+    @override
+    void dispose() {
+    super.dispose();
+    }
+
+    @override
+    Widget build(BuildContext context) {
+    _getBackInfo(context);
+    // TODO: implement build
+    return Scaffold(
+    appBar: AppBar(title: Text('详情页面'),),
+    body: Center(child: Text('详情页面商品详情${widget.goodsId}'),),
+    );
+    }
+
+
+    void _getBackInfo(BuildContext context)async{
 
     await Provide.value<DetailInfoProvide>(context).getGoodsInfor(widget.goodsId);
     print('加载完成');
-  }
+    }
 
 
-}
+    }
+ */
