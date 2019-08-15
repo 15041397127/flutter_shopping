c7a30c5a7a7dfbaa5fe489fc07c68413d44ca09f
zhangxu <zhangxusy@foxmail.com>
2019-08-15 15:01:25 +0800

    详情与评论切换

--- lib/pages/details_page/details_web.dart
+++ lib/pages/details_page/details_web.dart
@@ -8,16 +8,30 @@ class DetailsWeb extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    var goodsDetails = Provide.value<DetailInfoProvide>(context).goodsInfor.data.goodInfo.goodsDetail;
+    var goodsDetails = Provide.value<DetailInfoProvide>(context)
+        .goodsInfor
+        .data
+        .goodInfo
+        .goodsDetail;
 
+    return Provide<DetailInfoProvide>(
+      builder: (context, child, val) {
+        var isLeft = Provide.value<DetailInfoProvide>(context).isLeft;
+        if(isLeft){
 
-    return Container(
-
-       child: Html(
-         useRichText: false,
-           data: goodsDetails
-
-       ),
+          return Container(
+            child: Html(data: goodsDetails),
+          );
+        }else{
+          return Container(
+            width: ScreenUtil().setWidth(750),
+            padding: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: Text('暂时没有数据'),
+          );
+        }
+        
+      },
     );
   }
 }
